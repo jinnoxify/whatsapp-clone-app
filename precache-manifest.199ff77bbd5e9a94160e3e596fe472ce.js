f1cd1e1128d7767f265a837636fd73e923355bbf
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-10-21 21:33:37 -0500

    Updates

---
+++ precache-manifest.199ff77bbd5e9a94160e3e596fe472ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62eb37044b222a7c5a80873d7bd44a9b",
+    "url": "/whatsapp-clone-app/index.html"
+  },
+  {
+    "revision": "bdb72505bca6a618589b",
+    "url": "/whatsapp-clone-app/static/css/main.6f4b85f2.chunk.css"
+  },
+  {
+    "revision": "fe7cea6ff708525d4b15",
+    "url": "/whatsapp-clone-app/static/js/2.2f0798ad.chunk.js"
+  },
+  {
+    "revision": "822529ccff39843a00091842d366ba1b",
+    "url": "/whatsapp-clone-app/static/js/2.2f0798ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb72505bca6a618589b",
+    "url": "/whatsapp-clone-app/static/js/main.161b5068.chunk.js"
+  },
+  {
+    "revision": "a7deb02d46b4bad346c3",
+    "url": "/whatsapp-clone-app/static/js/runtime-main.e7faa365.js"
+  }
+]);
